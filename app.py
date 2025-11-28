7378adc33c885fb2616612397032b090dc28cd93
Joseph Samuel Angelo <159602624+josepsamuel2003@users.noreply.github.com>
2025-11-28 21:47:38 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=6913567cecda37cc1e302e3c1a053c8a"
+    response = requests.get(url)
+    data = response.json()
+
+    if "poster_path" in data and data["poster_path"]:
+        return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+    
+# langsung load karena file ada di folder yang sama dengan app.py
+movies = pickle.load(open("movies_list.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+movies_list = movies['title'].values
+
+st.header("Movie Recommender System")
+
+
+import os
+import streamlit.components.v1 as components
+
+# posisi app.py ada di Code/, jadi naik 1 folder ke frontend/public
+_component_dir = os.path.join(os.path.dirname(__file__), "frontend", "public")
+
+imageCarouselComponent = components.declare_component(
+    "image-carousel-component",
+    path=_component_dir
+)
+
+
+
+
+imageUrls = [
+    fetch_poster(1632),
+    fetch_poster(299536),
+    fetch_poster(17455),
+    fetch_poster(2830),
+    fetch_poster(429422),
+    fetch_poster(9722),
+    fetch_poster(13972),
+    fetch_poster(240),
+    fetch_poster(155),
+    fetch_poster(598),
+    fetch_poster(914),
+    fetch_poster(255709),
+    fetch_poster(572154)
+   
+    ]
+
+
+imageCarouselComponent(imageUrls=imageUrls, height=200)
+selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
+    for i in distance[1:6]:
+        movie_id = movies.iloc[i[0]].id
+        recommend_movie.append(movies.iloc[i[0]].title)
+        recommend_poster.append(fetch_poster(movie_id))
+    return recommend_movie, recommend_poster
+
+
+if st.button("Show Recommend"):
+    movie_name, movie_poster = recommend(selectvalue)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+        st.image(movie_poster[0])
+    with col2:
+        st.text(movie_name[1])
+        st.image(movie_poster[1])
+    with col3:
+        st.text(movie_name[2])
+        st.image(movie_poster[2])
+    with col4:
+        st.text(movie_name[3])
+        st.image(movie_poster[3])
+    with col5:
+        st.text(movie_name[4])
+        st.image(movie_poster[4])
